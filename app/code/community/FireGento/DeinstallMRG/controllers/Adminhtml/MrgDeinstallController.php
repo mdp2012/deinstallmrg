1e9fbbcbfb327365b2e25cb7da660158ca3c867c
Fabian Blechschmidt <Fabian.Blech@gmx.de>
2012-09-26 19:23:54 +0200

    fixed errors

--- app/code/community/FireGento/DeinstallMRG/controllers/Adminhtml/MrgDeinstallController.php
+++ app/code/community/FireGento/DeinstallMRG/controllers/Adminhtml/MrgDeinstallController.php
@@ -4,16 +4,6 @@ class FireGento_DeinstallMRG_Adminhtml_MrgDeinstallController
 {
     public function indexAction()
     {
-        if ($this->getRequest()->getMethod() == 'post') {
-            $methods = get_class_methods(get_class($this));
-            foreach ($methods as $method) {
-                if (strpos($method, 'uninstall') === 0) {
-                    $this->$method();
-                }
-            }
-        }
-
-
         $this->loadLayout();
         $formContainer = $this->getLayout()->createBlock(
             'firegento_deinstallmrg/mrgDeinstall_deinstall_form_container'
@@ -25,24 +15,39 @@ class FireGento_DeinstallMRG_Adminhtml_MrgDeinstallController
 
     public function deinstallAllAction()
     {
-
+        $this->removeModules(true);
     }
 
     public function deinstallPartlyAction()
     {
+        $this->removeModules();
+    }
 
+    protected function removeModules($force = false)
+    {
+        if ($this->getRequest()->getMethod() == 'post') {
+            $methods = get_class_methods(get_class($this));
+            foreach ($methods as $method) {
+                if (strpos($method, 'uninstall') === 0) {
+                    $this->$method($force);
+                }
+            }
+        }
     }
 
     /**
      * check wether option is set and remove file
      */
-    private function uninstallMageLocalWishlist()
+    private function uninstallMageLocalWishlist($force)
     {
-        if ($this->getRequest()->getParam('mage_local_wishlist_abstract')) {
+        if (
+            $this->getRequest()->getParam('mage_local_wishlist_abstract')
+            || $force
+        ) {
             // TODO check path
             unlink(
-                Mage::getBaseDir('code_dir') .
-                'local/Mage/Wishlist/Block/Abstract.php'
+                Mage::getBaseDir('code') .
+                '/local/Mage/Wishlist/Block/Abstract.php'
             );
         }
     }
